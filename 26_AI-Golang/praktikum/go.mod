1f6859c2102d43e3522131cf8495dbb0ed353f8b
Hafidz Naufal <91860668+hafidznaufl@users.noreply.github.com>
2023-10-16 07:41:04 +0700

    refa: 26 ai in go

--- 26_AI-Golang/praktikum/go.mod
+++ 26_AI-Golang/praktikum/go.mod
@@ -2,4 +2,8 @@ module ai
 
 go 1.21.0
 
-require github.com/sashabaranov/go-openai v1.16.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.5.1
+	github.com/sashabaranov/go-openai v1.16.0
+)
--- 26_AI-Golang/praktikum/main.go
+++ 26_AI-Golang/praktikum/main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"os"
 	"path/filepath"
 
 	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
 	openai "github.com/sashabaranov/go-openai"
 )
+
+type RecommendationRequest struct {
+	Budget  int    `json:"budget"`
+	Purpose string `json:"purpose"`
+}
+
+type RecommendationResponse struct {
+	Status string `json:"status"`
+	Data   string `json:"data"`
+}
 
 func main() {
 	err := godotenv.Load(filepath.Join(".", ".env"))
@@ -19,23 +31,56 @@ func main() {
 
 	TOKEN := os.Getenv("OPEN_AI_TOKEN")
 	client := openai.NewClient(TOKEN)
+
+	e := echo.New()
+
+	e.POST("/recommendation", func(c echo.Context) error {
+		req := new(RecommendationRequest)
+		if err := c.Bind(req); err != nil {
+			return c.JSON(http.StatusBadRequest, "Invalid request body")
+		}
+
+		// Generate the recommendation using OpenAI GPT-3
+		recommendation, err := generateRecommendation(client, req.Budget, req.Purpose)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, "Error generating recommendation")
+		}
+
+		response := RecommendationResponse{
+			Status: "success",
+			Data:   recommendation,
+		}
+
+		return c.JSON(http.StatusOK, response)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func generateRecommendation(client *openai.Client, budget int, purpose string) (string, error) {
+	// You can customize the user message based on the input data
+	userMessage := fmt.Sprintf("I have a budget of Rp %d and I need a %s laptop.", budget, purpose)
+
 	resp, err := client.CreateChatCompletion(
 		context.Background(),
 		openai.ChatCompletionRequest{
 			Model: openai.GPT3Dot5Turbo,
 			Messages: []openai.ChatCompletionMessage{
 				{
+					Role:    openai.ChatMessageRoleSystem,
+					Content: "You are a helpful assistant that provides laptop recommendations.",
+				},
+				{
 					Role:    openai.ChatMessageRoleUser,
-					Content: "Hello!",
+					Content: userMessage,
 				},
 			},
 		},
 	)
 
 	if err != nil {
-		fmt.Printf("ChatCompletion error: %v\n", err)
-		return
+		return "", err
 	}
 
-	fmt.Println(resp.Choices[0].Message.Content)
+	return resp.Choices[0].Message.Content, nil
 }
